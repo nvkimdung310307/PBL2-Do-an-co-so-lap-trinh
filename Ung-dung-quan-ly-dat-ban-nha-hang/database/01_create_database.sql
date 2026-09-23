ed4b0e7629161d0554da1ffd779d0211115b2d02
nvkimdung310307 <nvkimdung.310307@gmail.com>
2026-09-23 09:02:01 +0700

    Update 01_create_database.sql

--- Ung-dung-quan-ly-dat-ban-nha-hang/database/01_create_database.sql
+++ Ung-dung-quan-ly-dat-ban-nha-hang/database/01_create_database.sql
@@ -1,1 +1,21 @@
+USE master;
+GO
+-- Xóa database cũ nếu tồn tại
+IF DB_ID(N'RestaurantManagement') IS NOT NULL
+BEGIN
+    ALTER DATABASE RestaurantManagement
+    SET SINGLE_USER
+    WITH ROLLBACK IMMEDIATE;
 
+    DROP DATABASE RestaurantManagement;
+END
+GO
+  
+CREATE DATABASE RestaurantManagement;
+GO
+
+USE RestaurantManagement;
+GO
+
+PRINT N'Tạo database RestaurantManagement thành công!';
+GO
